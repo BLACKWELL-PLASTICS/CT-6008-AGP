eba6f5fc089fadf6abc3646daa572a1468425d6e
Anna Morgan <asm8@live.co.uk>
2022-02-21 13:03:42 +0000

    bt
    track 2 made
    bt started

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/PowerUpScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour
+{
+    private RocketTest RT;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Car")
+        {
+            RT = other.GetComponent<RocketTest>();
+            RT.poweredUp = true;
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/WayPointTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WayPointTest : MonoBehaviour
+{
+    public GameObject[] wayPoints;
+    public GameObject[] wayPoints2;
+    [Range(0.1f, 10)]
+    public float stoppingDist;
+    [Range(0, 20)]
+    public float speed;
+
+    private NavMeshAgent agent;
+    private Transform target;
+    private int counter = 0;
+    private float rand;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        rand = Random.Range(0, 2);
+    }
+
+    private void Update()
+    {
+        
+        if(rand == 0)
+        {
+            agent.speed = speed;
+            float dist = Vector3.Distance(transform.position, wayPoints[counter].transform.position);
+            if (dist <= stoppingDist)
+            {
+                counter++;
+                if (counter >= wayPoints.Length)
+                {
+                    counter = 0;
+                }
+            }
+            target = wayPoints[counter].transform;
+            agent.SetDestination(target.position);
+        }
+        else if (rand == 1)
+        {
+            agent.speed = speed;
+            float dist = Vector3.Distance(transform.position, wayPoints2[counter].transform.position);
+            if (dist <= stoppingDist)
+            {
+                counter++;
+                if (counter >= wayPoints2.Length)
+                {
+                    counter = 0;
+                }
+            }
+            target = wayPoints2[counter].transform;
+            agent.SetDestination(target.position);
+        }
+        
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, speed*Time.deltaTime);
+        
+    }
+
+    //private int FindClosest()
+    //{
+    //    int counter = 0;
+    //    float tempClostest = Vector3.Distance(this.transform.position, wayPoints[0].transform.position);
+    //    for (int i = 0; i < wayPoints.Length; i++)
+    //    {
+    //        float distance = Vector3.Distance(this.transform.position, wayPoints[i].transform.position);
+    //        if (distance < tempClostest)
+    //        {
+    //            tempClostest = distance;
+    //            counter = i;
+    //        }
+    //    }
+
+    //    return counter;
+    //}
+
+}
