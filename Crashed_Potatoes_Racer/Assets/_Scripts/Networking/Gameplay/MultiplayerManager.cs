fcd0656091a03aea439f5a002e1632936ec55ef9
Iain2019 <55756106+Iain2019@users.noreply.github.com>
2022-02-24 16:46:19 +0000

    GameplayNetworkingStart

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/Networking/Gameplay/MultiplayerManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Networking.Transport;
+using UnityEngine;
+
+public class MultiplayerManager : MonoBehaviour
+{
+    public List<GameObject> m_activeCars;
+
+    [SerializeField]
+    GameObject m_DivableCar;
+    [SerializeField]
+    GameObject m_onlineCar;
+    [SerializeField]
+    GameObject[] m_startPoints;
+
+    private void Start()
+    {
+        for (int i = 0; i < PersistentInfo.Instance.m_connectedUsers; i++)
+        {
+            Vector3 pos = m_startPoints[i].transform.position;
+            Quaternion rot = m_startPoints[i].transform.rotation;
+
+            if (i == PersistentInfo.Instance.m_currentPlayerNum - 1)
+            {
+                Instantiate(m_DivableCar, pos, rot);
+            }
+            else
+            {
+                Instantiate(m_onlineCar, pos, rot);
+            }
+        }
+    }
+
+    void Awake()
+    {
+        RegisterEvenets();
+    }
+
+    void RegisterEvenets()
+    {
+        //Server
+        NetUtility.S_MAKE_MOVE += OnMoveServer;
+
+        //Client
+        NetUtility.C_MAKE_MOVE += OnMoveClient;
+    }
+    void UnregisterEvenets()
+    {
+
+    }
+
+    //Server
+    void OnMoveServer(NetMessage a_msg, NetworkConnection a_connection)
+    {
+        NetMakeMove netMakeMove = a_msg as NetMakeMove;
+        Server.Instance.Broadcast(netMakeMove);
+    }
+
+    //Client
+    void OnMoveClient(NetMessage a_msg)
+    {
+        NetMakeMove netMakeMove = a_msg as NetMakeMove; 
+        if (netMakeMove.m_Player != PersistentInfo.Instance.m_currentPlayerNum)
+        {
+            m_activeCars[netMakeMove.m_Player - 1].transform.position = new Vector3(netMakeMove.m_XPos, netMakeMove.m_YPos, netMakeMove.m_ZPos);
+            m_activeCars[netMakeMove.m_Player - 1].transform.rotation = new Quaternion(netMakeMove.m_XRot, netMakeMove.m_YRot, netMakeMove.m_ZRot, netMakeMove.m_WRot);
+        }
+    }
+}
---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/Networking/Gameplay/UpdateMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NetMakeMove netMakeMove = new NetMakeMove();
+        netMakeMove.m_Player = PersistentInfo.Instance.m_currentPlayerNum;
+        netMakeMove.m_XPos = gameObject.transform.position.x;
+        netMakeMove.m_YPos = gameObject.transform.position.y;
+        netMakeMove.m_ZPos = gameObject.transform.position.z;
+        netMakeMove.m_XRot = gameObject.transform.rotation.x;
+        netMakeMove.m_YRot = gameObject.transform.rotation.y;
+        netMakeMove.m_ZRot = gameObject.transform.rotation.z;
+        netMakeMove.m_WRot = gameObject.transform.rotation.w;
+        Client.Instance.SendToServer(netMakeMove);
+    }
+}
