0e04c993fde1f5224d0ac485b53406312c1e78bb
s1903628 <s1903628@glos.ac.uk>
2022-03-09 10:53:20 +0000

    Kart Selection Scene and General UI
    Created a new scene that is a menu in which players select their kart. An "Animations" and "Sprites" folder was created for the UI elements, and TextMeshPro was also installed. A basic script was done that updates the kart model's material when the player selects a different option (though this will be replaced by a different prefab instead of a different material).

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/MenuScripts/KartPreview.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KartPreview : MonoBehaviour, ISelectHandler
+{
+    public GameObject targetObject;
+    public Material objectMaterial;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        targetObject.GetComponent<Renderer>().material = objectMaterial;
+    }
+}
