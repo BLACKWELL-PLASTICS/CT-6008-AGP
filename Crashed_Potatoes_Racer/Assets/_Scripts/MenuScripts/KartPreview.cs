4808ffcb901ae13f91ad5b48cb126944708e2a79
s1903628 <s1903628@glos.ac.uk>
2022-03-16 15:53:45 +0000

    Addition of Wheel Menu and Mouse Controls
    Included a second menu in which the players can choose wheels for their vehicle. Also added a stats menu to the left of the screen that displays what you have chosen. Finally, before you had to use WASD to navigate the buttons, now you can with mouse controls too.

--- Crashed_Potatoes_Racer/Assets/_Scripts/MenuScripts/KartPreview.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/MenuScripts/KartPreview.cs
@@ -1,13 +1,37 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class KartPreview : MonoBehaviour, ISelectHandler
 {
-    public GameObject targetObject;
+    [SerializeField]
+    private GameObject targetObject;
     public Material objectMaterial;
+
+    [SerializeField]
+    private TextMeshProUGUI[] targetText;
+    public string[] textUpdate;
 
     public void OnSelect(BaseEventData eventData)
     {
+        //When the player uses WASD, arrow keys or game controller to navigate the menu
+        UpdateSelection();
+    }
+
+    public void OnHover(BaseEventData eventData)
+    {
+        //When the player uses a mouse cursor to navigate the menu
+        UpdateSelection();
+    }
+
+    void UpdateSelection()
+    {
+        //Updates the model and text when the player selects a customisable option
         targetObject.GetComponent<Renderer>().material = objectMaterial;
+
+        for (int i = 0; i < textUpdate.Length; i++)
+        {
+            targetText[i].text = textUpdate[i];
+        }
     }
 }
