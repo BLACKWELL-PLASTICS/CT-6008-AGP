5f08aaecbcc0547159be5cb8d2c8f3b1dbeb1ed6
Anna Morgan <asm8@live.co.uk>
2022-03-19 01:17:08 +0000

    i actually dont know
    kinda just looked and tested stuff

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/NavMeshBaker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+
+public class NavMeshBaker : MonoBehaviour
+{
+    private NavMeshSurface[] walkableArea;
+    private void Awake()
+    {
+        walkableArea = FindObjectsOfType<NavMeshSurface>();
+
+        for(int i = 0; i < walkableArea.Length; i++)
+        {
+            walkableArea[i].BuildNavMesh();
+        }
+    }
+}
