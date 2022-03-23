58452db6abc3392ca4362f198cc3909b4adf0d95
Oli Blackwell <34063887+BUISCUI7MUNCHER@users.noreply.github.com>
2022-03-23 17:06:16 +0000

    Getting powerups ready for packets

--- Crashed_Potatoes_Racer/Assets/_Scripts/Powerups/Obsticle.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/Powerups/Obsticle.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Obsticle : MonoBehaviour {
     public GameObject prefab;
     GameObject wall;
-    float timer;
-    bool isSpawned = false;
-
     Vector3 eulerAngle;
     Quaternion currentRot;
 
@@ -16,20 +13,5 @@ public class Obsticle : MonoBehaviour {
         eulerAngle = new Vector3(-90f, 0f, 0f);
         currentRot.eulerAngles = eulerAngle;
         wall.transform.rotation = currentRot;
-        isSpawned = true;
-    }
-
-    // Update is called once per frame
-    void Update() {
-        if (isSpawned == true) {
-            timer += Time.deltaTime;
-            wall.transform.localScale = wall.transform.localScale * 1.0009f;
-
-            if (timer > 5f) {
-                isSpawned = false;
-                Destroy(wall);
-                Destroy(gameObject.GetComponent<Obsticle>());
-            }
-        }
     }
 }
---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/Powerups/ObsticleScaler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticleScaler : MonoBehaviour
+{
+    float timer;
+
+    private void Awake() {
+        // send packet of position and rotation
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        transform.localScale = transform.localScale * 1.0009f;
+
+        if (timer > 5f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
