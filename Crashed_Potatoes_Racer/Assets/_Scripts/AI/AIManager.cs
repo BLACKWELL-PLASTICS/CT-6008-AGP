575c73a75a048b35056ab1e8da313ea6b33f0771
Anna Morgan <asm8@live.co.uk>
2022-03-27 23:07:52 +0100

    BOIDS BOIDS BOIDS

--- Crashed_Potatoes_Racer/Assets/_Scripts/AI/AIManager.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/AIManager.cs
@@ -4,39 +4,76 @@ using UnityEngine;
 
 public class AIManager : MonoBehaviour
 {
+    private static AIManager instance;
+
+    //car ai
+    [Header("Car Variables")]
     public Transform[] wayPoints;
-   // public Transform[] spawnLocation;
-   // public GameObject AICarPrefab;
     public float maxSpeed;
     public float maxAcc;
     public float speedIncrease;
     public float speedDecrease;
     public float slowDownPeriod;
     public float stoppingRay;
+    //boid variables
+    [Header("Boid Variables")]
+    public GameObject boidPrefab;
+    public Transform boidLocation;
+    public int amountBoids;
+    public float areaLimit;
+    public float maxBoidSpeed;
+    public float rotationSpeed;
+    public float neighbourDist;
+    public float goalRandomness;
+    [HideInInspector]
+    public Vector3 goalPos;
+    [HideInInspector]
+    public GameObject[] boidArray;
 
-
-    private static AIManager instance;
+    //car get/set
     public static float GetMaxAcc { get { return instance.maxAcc; } }
     public static float GetMaxSpeed { get { return instance.maxSpeed; } }
     public static float GetIncrease { get { return instance.speedIncrease; } }
     public static float GetDecrease { get { return instance.speedDecrease; } }
     public static float GetSlowDownPeriod { get { return instance.slowDownPeriod; } }
     public static float GetStoppingRay { get { return instance.stoppingRay; } }
+    public static Transform[] GetWaypoints { get { return instance.wayPoints; } }
 
-
-
-    public static Transform[] GetWaypoints {get {return instance.wayPoints; } }
-    //public static Transform[] GetCorners { get { return instance.corners; } }
-
-    public static float LowerSpeed(float currentSpeed) { currentSpeed -= 4; return currentSpeed; }
+    //boid get/set
+    public static float GetMaxBoidSpeed { get { return instance.maxBoidSpeed; } }
+    public static float GetAreaLimit { get { return instance.areaLimit; } }
+    public static float GetBoidRotation { get { return instance.rotationSpeed; } }
+    public static float GetNeighbourDist { get { return instance.neighbourDist; } }
+    public static Vector3 GetGoalPos { get { return instance.goalPos; } }
+    public static GameObject[] GetBoids { get { return instance.boidArray; } }
+    public static Transform GetBoidStart { get { return instance.boidLocation; } }
 
     private void Awake()
     {
         instance = this;
-        //for(int i = 0; i < 2; i++)
-        //{
-        //    Instantiate(AICarPrefab, spawnLocation[i].position, Quaternion.identity);
 
-        //}
+        boidArray = new GameObject[amountBoids];
+
+        for (int i = 0; i < amountBoids; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(boidLocation.position.x - areaLimit, boidLocation.position.x + areaLimit), 
+                                           Random.Range(boidLocation.position.y -areaLimit, boidLocation.position.y + areaLimit), 
+                                           Random.Range(boidLocation.position.z -areaLimit, boidLocation.position.z + areaLimit));
+            boidArray[i] = (GameObject)Instantiate(boidPrefab, position, Quaternion.identity); 
+            boidArray[i].GetComponent<BoidController>().manager = this; 
+        }
+    }
+    private void Update()
+    {
+        GoalPosRandom();   
+    }
+    private void GoalPosRandom()
+    {
+        if (Random.Range(0, goalRandomness) < 50)
+        {
+            goalPos = new Vector3(Random.Range(boidLocation.position.x - areaLimit, boidLocation.position.x + areaLimit),
+                                  Random.Range(boidLocation.position.y - areaLimit, boidLocation.position.y + areaLimit),
+                                  Random.Range(boidLocation.position.z - areaLimit, boidLocation.position.z + areaLimit));
+        }
     }
 }
---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/EnvirnmentAI/BoidController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidController : MonoBehaviour
+{
+    private float speed;
+    [HideInInspector]
+    public AIManager manager;
+
+    void Start()
+    {
+        speed = Random.Range(1, AIManager.GetMaxBoidSpeed);
+    }
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, AIManager.GetBoidStart.position) >= AIManager.GetAreaLimit)
+        {
+            Vector3 direction = AIManager.GetBoidStart.position - transform.position; 
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), AIManager.GetBoidRotation * Time.deltaTime); 
+            speed = Random.Range(1, 15); 
+        }
+        else
+        {
+            if (Random.Range(0, 5) < 1) 
+            {
+                ApplyRules(); 
+            }
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed); 
+    }
+
+    private void ApplyRules()
+    {
+        GameObject[] otherBoids;
+        otherBoids = AIManager.GetBoids; 
+
+        Vector3 center = Vector3.zero; 
+        Vector3 avoid = Vector3.zero; 
+        Vector3 goalPos = AIManager.GetGoalPos;
+
+        float distance; 
+        int groupSize = 0; 
+        float averageSpeed = 0.1f; 
+
+        foreach (GameObject i in otherBoids) 
+        {
+            if (i != this.gameObject) 
+            {
+                distance = Vector3.Distance(i.transform.position, this.transform.position); 
+                if (distance <= AIManager.GetNeighbourDist) 
+                {
+                    center += i.transform.position; 
+                    groupSize++; 
+
+                    if (distance < 1.5) 
+                    {
+                        avoid = avoid + (this.transform.position - i.transform.position); 
+                    }
+                    BoidController anotherFlock = i.GetComponent<BoidController>(); 
+                    averageSpeed += anotherFlock.speed;
+                }
+            }
+        }
+        if (groupSize > 0)
+        {
+            center = center / groupSize + (goalPos - this.transform.position);
+            speed = averageSpeed / groupSize;
+
+            Vector3 direction = (center + avoid) - transform.position;
+            if (direction != AIManager.GetBoidStart.position)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), AIManager.GetBoidRotation * Time.deltaTime);
+
+        }
+
+    }
+}
