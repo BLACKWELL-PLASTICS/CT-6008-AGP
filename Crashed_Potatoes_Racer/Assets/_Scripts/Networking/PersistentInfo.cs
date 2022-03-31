4797f07c5a4d33236e6d53e3e4e5143fd9941276
Iain2019 <55756106+Iain2019@users.noreply.github.com>
2022-03-31 22:59:42 +0100

    Pre Merge

--- Crashed_Potatoes_Racer/Assets/_Scripts/Networking/PersistentInfo.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/Networking/PersistentInfo.cs
@@ -9,17 +9,41 @@ class PersistentInfo : MonoBehaviour
     public List<string> m_connectedNames { get; set; }
     public int m_currentPlayerNum { get; set; }
     public string m_currentPlayerName { get; set; }
+    //public CarDesigns[] m_carDesigns { get; set; }
 
     public static PersistentInfo Instance { set; get; }
     void Awake()
     {
         Instance = this;
         m_connectedNames = new List<string>();
+        //m_carDesigns = new CarDesigns[8];
+        //foreach (CarDesigns carDesigns in m_carDesigns)
+        //{
+        //    carDesigns.Initialise();
+        //}
     }
     public void Clear()
     {
         m_connectedUsers = 0;
         m_connectedNames.Clear();
         m_currentPlayerNum = 0;
+        //foreach (CarDesigns carDesigns in m_carDesigns)
+        //{
+        //    carDesigns.Initialise();
+        //}
     }
 }
+
+//public class CarDesigns
+//{
+//    public int m_carChoice = 0;
+//    public int m_wheelChoice = 0;
+//    public int m_gunChoice = 0;
+
+//    public void Initialise()
+//    {
+//        m_carChoice = 0;
+//        m_wheelChoice = 0;
+//        m_gunChoice = 0;
+//    }
+//}
