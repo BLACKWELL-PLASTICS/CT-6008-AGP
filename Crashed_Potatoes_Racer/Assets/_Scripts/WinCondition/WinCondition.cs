bf126e23387bde2989c6a30d51cc3368ef81dd0e
BUISCUI7MUNCHER <oliver.blackwell@hotmail.co.uk>
2022-04-11 10:39:04 +0100

    Start Win Condition

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/WinCondition/WinCondition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    [SerializeField] List<GameObject> winMarkers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
