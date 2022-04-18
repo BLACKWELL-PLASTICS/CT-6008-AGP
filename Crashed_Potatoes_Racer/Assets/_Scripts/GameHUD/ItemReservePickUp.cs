0ee2c3783bdcd6560c73bc100c348e134eac58d6
s1903628 <s1903628@glos.ac.uk>
2022-04-18 16:29:53 +0100

    Temp. Splat Animation and Other UI Stuff

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/GameHUD/ItemReservePickUp.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ItemReservePickUp : MonoBehaviour
+{
+    public Animator powerUpAnimator;
+
+    void Update()
+    {
+        //Debugging purposes; replace with Oli's code when the player picks up a power-up and call "ItemPickup()"
+        switch (Input.inputString)
+        {
+            case "9":
+                ItemPickup();
+                break;
+        }
+    }
+
+    public void ItemPickup()
+    {
+        powerUpAnimator.SetTrigger("itemPickup");
+    }
+}
---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/GameHUD/SplatScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplatScreen : MonoBehaviour
+{
+    public Animator splatAnimator;
+
+    void Start()
+    {
+        splatAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Debugging purposes; replace with Oli's code when the player is hit with the power-up and call "SplatAnimation()"
+        switch (Input.inputString)
+        {
+            case "0":
+                SplatAnimation();
+                break;
+        }
+    }
+
+    public void SplatAnimation()
+    {
+        splatAnimator.SetTrigger("coverScreen");
+    }
+}
