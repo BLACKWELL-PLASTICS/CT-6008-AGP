dbab714e9a938d49f76e0b3c69d2cce979764edf
Anna Morgan <asm8@live.co.uk>
2022-05-03 00:39:24 +0100

    Ai manager swap around ready for lvl 2

--- Crashed_Potatoes_Racer/Assets/_Scripts/AI/AIManager.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/AIManager.cs
@@ -19,31 +19,6 @@ public class AIManager : MonoBehaviour
     public float stoppingRay;
     public float detectionRay;
     public float detectionAngle;
-    //boid variables
-    [Header("Boid Variables")]
-    public GameObject boidPrefab;
-    public Transform boidLocation;
-    public int amountBoids;
-    public float areaLimit;
-    public float maxBoidSpeed;
-    public float rotationSpeed;
-    public float neighbourDist;
-    public float goalRandomness;
-    [HideInInspector]
-    public Vector3 goalPos;
-    [HideInInspector]
-    public GameObject[] boidArray;
-    //fish variables
-    [Header("Fish Variables")]
-    public float fishRandomnessSea;
-    public float fishRandomnessWater;
-    public float fishSpeed;
-    public float fishJumpTime;
-    //Crab variables
-    [Header("Crab Variables")]
-    public float crabDistance;
-    public float crabTimer;
-    public float crabSpeed;
 
     //car get/set
     public static float GetMaxAcc { get { return instance.maxAcc; } }
@@ -58,53 +33,11 @@ public class AIManager : MonoBehaviour
     public static Transform[] GetWaypoints3 { get { return instance.wayPoints3; } }
     public static GameObject[] GetPowerUp { get { return instance.powerUps; } }
 
-    //boid get/set
-    public static float GetMaxBoidSpeed { get { return instance.maxBoidSpeed; } }
-    public static float GetAreaLimit { get { return instance.areaLimit; } }
-    public static float GetBoidRotation { get { return instance.rotationSpeed; } }
-    public static float GetNeighbourDist { get { return instance.neighbourDist; } }
-    public static Vector3 GetGoalPos { get { return instance.goalPos; } }
-    public static GameObject[] GetBoids { get { return instance.boidArray; } }
-    public static Transform GetBoidStart { get { return instance.boidLocation; } }
-
-    //fish get/set
-    public static float GetFishRandomnessSea { get { return instance.fishRandomnessSea; } }
-    public static float GetFishRandomnessWater { get { return instance.fishRandomnessWater; } }
-    public static float GetFishSpeed { get { return instance.fishSpeed; } }
-    public static float GetFishTime { get { return instance.fishJumpTime; } }
-    //crabs get/set
-    public static float GetCrabDistance { get { return instance.crabDistance; } }
-    public static float GetCrabTimer { get { return instance.crabTimer; } }
-    public static float GetCrabSpeed { get { return instance.crabSpeed; } }
-
-
 
     private void Awake()
     {
         instance = this;
-
-        boidArray = new GameObject[amountBoids];
-
-        for (int i = 0; i < amountBoids; i++)
-        {
-            Vector3 position = new Vector3(Random.Range(boidLocation.position.x - areaLimit, boidLocation.position.x + areaLimit), 
-                                           Random.Range(boidLocation.position.y -areaLimit, boidLocation.position.y + areaLimit), 
-                                           Random.Range(boidLocation.position.z -areaLimit, boidLocation.position.z + areaLimit));
-            boidArray[i] = (GameObject)Instantiate(boidPrefab, position, Quaternion.identity); 
-            boidArray[i].GetComponent<BoidController>().manager = this; 
-        }
+               
     }
-    private void Update()
-    {
-        GoalPosRandom();   
-    }
-    private void GoalPosRandom()
-    {
-        if (Random.Range(0, goalRandomness) < 50)
-        {
-            goalPos = new Vector3(Random.Range(boidLocation.position.x - areaLimit, boidLocation.position.x + areaLimit),
-                                  Random.Range(boidLocation.position.y - areaLimit, boidLocation.position.y + areaLimit),
-                                  Random.Range(boidLocation.position.z - areaLimit, boidLocation.position.z + areaLimit));
-        }
-    }
+    
 }
--- Crashed_Potatoes_Racer/Assets/_Scripts/AI/EnvirnmentAI/CrabScript.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/EnvirnmentAI/CrabScript.cs
@@ -5,32 +5,50 @@ using UnityEngine.AI;
 
 public class CrabScript : MonoBehaviour
 {
+    public float crabDistance = 3;
+    public float crabTimer = 5;
+    public float crabSpeed = 2;
+
     private float timer = 0;
     private NavMeshAgent crab;
+    private Animator anim;
+    private Vector3 newPos;
     private void Start()
     {
         crab = GetComponent<NavMeshAgent>();
-        crab.speed = AIManager.GetCrabSpeed;
+        anim = GetComponentInChildren<Animator>();
+        crab.speed = crabSpeed;
     }
     private void Update()
     {
         timer += Time.deltaTime;
-        if (timer >= AIManager.GetCrabTimer)
+        if (timer >= crabTimer)
         {
-            Vector3 newPos = RandomPoint(transform.position);
+            newPos = RandomPoint(transform.position);
             crab.SetDestination(newPos);
             timer = 0;
         }
+
+        if(transform.position == newPos)
+        {
+            crab.isStopped = true;
+            anim.SetBool("Walking", false);
+        }
+        else
+        {
+            crab.isStopped = false;
+            anim.SetBool("Walking", true);
+        }
     }
-    private static Vector3 RandomPoint(Vector3 origin)
+    private Vector3 RandomPoint(Vector3 origin)
     {
-        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * AIManager.GetCrabDistance;
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * crabDistance;
 
         randomDirection += origin;
 
         NavMeshHit navHit;
 
-        NavMesh.SamplePosition(randomDirection, out navHit, AIManager.GetCrabDistance, 9);
+        NavMesh.SamplePosition(randomDirection, out navHit, crabDistance, 9);
 
         return navHit.position;
     }
