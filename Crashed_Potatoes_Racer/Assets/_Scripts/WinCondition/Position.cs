cdee1d1c4805cfb5a67706840d111ee68474e445
Oli Blackwell <34063887+BUISCUI7MUNCHER@users.noreply.github.com>
2022-05-04 00:09:10 +0100

    Position Data continue

--- Crashed_Potatoes_Racer/Assets/_Scripts/WinCondition/Position.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/WinCondition/Position.cs
@@ -6,6 +6,10 @@ public class Position : MonoBehaviour
 {
     public int currentPosition;
 
+    private void Start() {
+        currentPosition = GetComponent<CarManagerScript>().m_playerNum;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -14,6 +18,28 @@ public class Position : MonoBehaviour
         }
         if (currentPosition > 8) {
             currentPosition = 8;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            // if the first checkpoints not been gone past
+            if (GetComponent<WinCondition>().checkpointNumber < 0) {
+                return;
+            }
+            // Check if on the same lap
+            if (other.gameObject.GetComponent<WinCondition>().lap != GetComponent<WinCondition>().lap) {
+                return;
+            } else { // On the same lap
+                GameObject checkpoint = GetComponent<WinCondition>().array[GetComponent<WinCondition>().checkpointNumber];
+                float carOneDistance = Vector3.Distance(checkpoint.transform.position, transform.position);
+                float carTwoDistance = Vector3.Distance(checkpoint.transform.position, other.transform.position);
+                if (carOneDistance < carTwoDistance) { // this car is closer
+                    currentPosition++;
+                } else {
+                    currentPosition--;
+                }
+            }
         }
     }
 
