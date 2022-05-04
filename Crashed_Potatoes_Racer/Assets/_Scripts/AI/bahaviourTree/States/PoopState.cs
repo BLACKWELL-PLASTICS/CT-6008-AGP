2b40547128cc8e8b9dbc61fcd90c3a54084aa3e7
Anna Morgan <asm8@live.co.uk>
2022-05-04 23:43:47 +0100

    stuff
    power ups done maybe
    sound

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/AI/bahaviourTree/States/PoopState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoopState : Node
+{
+    public PoopState(AIPlayer owner) : base(owner)
+    {
+
+    }
+
+    public override NodeState Update()
+    {
+        NetBirdPoop netBirdPoop = new NetBirdPoop();
+        netBirdPoop.m_Player = owner.GetComponent<CarManagerScript>().m_playerNum;
+        Client.Instance.SendToServer(netBirdPoop);
+        owner.InventoryComponent.UsePowerup();
+        return NodeState.SUCCESS;
+    }
+}
+
