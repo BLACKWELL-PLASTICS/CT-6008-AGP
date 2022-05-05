b5f01fa88346eb564eb704e304ccc8bad3937aff
Oli Blackwell <34063887+BUISCUI7MUNCHER@users.noreply.github.com>
2022-05-05 15:44:49 +0100

    Bug, but not real bug

--- Crashed_Potatoes_Racer/Assets/_Scripts/GameHUD/ItemReservePickUp.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/GameHUD/ItemReservePickUp.cs
@@ -6,31 +6,30 @@ public class ItemReservePickUp : MonoBehaviour {
 
     public Sprite[] images;
 
-    public Sprite current;
+    public Image current;
 
     void Update() {
 
         switch (GameObject.Find("Car_Reg(Clone)").GetComponent<InventoryScript>().p1) {
             case SeedPacketScript.POWERUPS.Forward_Projectile:
-
+                current.sprite = images[3];
                 break;
             case SeedPacketScript.POWERUPS.Blind:
-
+                current.sprite = images[2];
                 break;
             case SeedPacketScript.POWERUPS.Boost:
-
+                current.sprite = images[0];
                 break;
             case SeedPacketScript.POWERUPS.Gum:
-
+                current.sprite = images[5];
                 break;
             case SeedPacketScript.POWERUPS.Obsticles:
-
+                current.sprite = images[1];
                 break;
             case SeedPacketScript.POWERUPS.Size_Increase:
-
+                current.sprite = images[4];
                 break;
             case SeedPacketScript.POWERUPS.Hot_Potato:
-
                 break;
         }
 
