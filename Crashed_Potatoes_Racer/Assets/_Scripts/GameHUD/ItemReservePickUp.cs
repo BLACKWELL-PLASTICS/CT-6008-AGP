86aaaa8848c5675a676d8feba2e4feda967fe329
Oli Blackwell <34063887+BUISCUI7MUNCHER@users.noreply.github.com>
2022-05-05 15:01:43 +0100

    kms

--- Crashed_Potatoes_Racer/Assets/_Scripts/GameHUD/ItemReservePickUp.cs
+++ Crashed_Potatoes_Racer/Assets/_Scripts/GameHUD/ItemReservePickUp.cs
@@ -1,22 +1,42 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class ItemReservePickUp : MonoBehaviour
-{
+public class ItemReservePickUp : MonoBehaviour {
     public Animator powerUpAnimator;
 
-    void Update()
-    {
-        //Debugging purposes; replace with Oli's code when the player picks up a power-up and call "ItemPickup()"
-        switch (Input.inputString)
-        {
-            case "9":
-                ItemPickup();
+    public Sprite[] images;
+
+    public Sprite current;
+
+    void Update() {
+
+        switch (GameObject.Find("Car_Reg(Clone)").GetComponent<InventoryScript>().p1) {
+            case SeedPacketScript.POWERUPS.Forward_Projectile:
+
+                break;
+            case SeedPacketScript.POWERUPS.Blind:
+
+                break;
+            case SeedPacketScript.POWERUPS.Boost:
+
+                break;
+            case SeedPacketScript.POWERUPS.Gum:
+
+                break;
+            case SeedPacketScript.POWERUPS.Obsticles:
+
+                break;
+            case SeedPacketScript.POWERUPS.Size_Increase:
+
+                break;
+            case SeedPacketScript.POWERUPS.Hot_Potato:
+
                 break;
         }
+
     }
 
-    public void ItemPickup()
-    {
+    public void ItemPickup() {
         powerUpAnimator.SetTrigger("itemPickup");
     }
 }
