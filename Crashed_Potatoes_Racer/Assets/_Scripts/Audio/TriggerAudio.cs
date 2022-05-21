b3e045d9b4c4da2224a53861ffbd8a589e3b9416
Anna Morgan <asm8@live.co.uk>
2022-05-21 22:06:26 +0100

    more sounds in
    even made a fancy audio script

---
+++ Crashed_Potatoes_Racer/Assets/_Scripts/Audio/TriggerAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    public FMODUnity.EventReference Event;
+    public bool PlayOnAwake;
+
+    void Start()
+    {
+        if(PlayOnAwake)
+        {
+            PlayOneShot();
+        }
+    }
+
+    public void PlayOneShot()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(Event.Path, gameObject);
+    }
+}
